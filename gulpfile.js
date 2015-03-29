88f4817453afd445ff647cc9a287a5e0f20a7b12
Ben Lau <xbenlau@gmail.com>
2015-03-29 18:51:34 +0800

    gulpfile.js : Added style , clean and assets task

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,36 @@
-require('./gulp');
+var gulp = require('gulp'),
+    del = require('del'),
+    concat = require('gulp-concat');
+
+
+var BUILD_FOLDER = 'build'
+
+
+gulp.task('clean', function (cb) {
+    return del(BUILD_FOLDER,cb);
+});
+
+gulp.task('style',function() {
+    
+    return gulp.src(["src/chrome/*.css",
+                     "src/chrome/**/*.css",
+                     "!src/chrome/debug/**/*.css"
+                    ])
+            .pipe(concat('app.css'))
+            .pipe(gulp.dest(BUILD_FOLDER));
+
+});
+
+gulp.task('assets',function() {
+   return gulp.src(["src/chrome/*.html",
+             "src/chrome/css/*",
+             "src/chrome/**/*.css",
+             "src/chrome/lib/*",             
+             "src/chrome/*.json",
+             "src/chrome/*.png"
+            ],{
+                 base : "./src/chrome"
+             }
+           ).pipe(gulp.dest(BUILD_FOLDER));    
+});
+
